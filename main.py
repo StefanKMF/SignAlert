40e4b4f5b088a9f71ca013b7a4cec5de131320df
Stefan M-F <StefanKMF@users.noreply.github.com>
2015-08-06 13:16:13 -0400

    Added ability to produce RSS/Atom Feeds, and serialize data.

--- main.py
+++ main.py
@@ -2,25 +2,34 @@ from bs4 import BeautifulSoup
 from feedgen.feed import FeedGenerator
 import urllib2, json
 
-
-def exportToRSS(updates):
-    for sign in updates:
-        fg = FeedGenerator()
-        fg.title(sign + ' Posted')
-        fg.link( href='http://ottawa.ca/en/residents/transportation-and-parking/traffic/decommissioned-street-name-signs')
-        fg.subtitle('A new decommissioned street sign has been posted in Ottawa.')
-        fg.language('en')
-        rssfeed = fg.rss_str(pretty=True)
-        fg.rss_file('rss.xml')
+fg = FeedGenerator()
 
 
-def main():
+def createRSS():
+    fg.title('Decommissioned Street Sign Feed')
+    fg.link( href='http://ottawa.ca/en/residents/transportation-and-parking/traffic/decommissioned-street-name-signs')
+    fg.subtitle('Lets you know when signs are posted.')
+    fg.language('en')
+    rssfeed = fg.rss_str(pretty=True)
+
+def exportRSS(Signs):
+    for sign in Signs:
+        fe = fg.add_entry()
+        fe.title(sign + ' Posted')
+    fg.rss_file('rss.xml')
+    #fg.atom_file('atom.xml')
+
+def getNew(Signs):
+    print "empty"
+
+
+def getSigns():
+
     response = urllib2.urlopen('http://ottawa.ca/en/residents/transportation-and-parking/traffic/decommissioned-street-name-signs')
     html = response.read()
 
     soup = BeautifulSoup(html)
     tables = soup.findAll("table")
-
 
     Signs = {}
 
@@ -31,8 +40,17 @@ def main():
             Signs[(cells[0].get_text().encode('ascii','ignore')).translate(None, '\n\t\r')] = (cells[1].get_text().encode('ascii','ignore')).translate(None, '\n\t\r')
     del Signs['Street Name']
 
-    exportToRSS(Signs)
+    with open('signs.json', 'w') as fileOpen:
+        json.dump(Signs, fileOpen)
 
+    return Signs
+
+
+def main():
+
+    Signs = getSigns()
+    createRSS()
+    exportRSS(Signs)
 
 
 
